6336ffe29bc6a26c39443c24b84c5d0b16b97b07
Jason <jasmead@gmail.com>
2010-11-20 22:47:36 -0600

    Add Service resolver interfaces

---
+++ src/Ninterfaces.IOC/IGenericServiceResolver.cs
@@ -0,0 +1,16 @@
+﻿namespace Ninterfaces.IOC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public interface IGenericServiceResolver
+    {
+        T Resolve<T>();
+
+        T Resolve<T>(string name);
+
+        IEnumerable<T> ResolveAll<T>();
+    }
+}
